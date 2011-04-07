78408db08d8c7860ad0fa13d0dbc7cf200fa6574
Michael Rogger <michael.rogger@sti2.at>
2011-04-07 17:25:55 +0200

    intitial commit of invoker-grounding

---
+++ invoker-grounding/src/main/java/at/sti2/ngsee/invoker_grounding/App.java
@@ -0,0 +1,13 @@
+package at.sti2.ngsee.invoker_grounding;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
