4badb92194a95d2734f572b8d7b6505534300367
Corneliu Stanciu <stanciucorneliu@networld.to>
2011-04-28 12:17:36 +0200

    Fixing the activation exception after using the CXF and AXIS2

---
+++ invoker-framework/src/main/java/at/sti2/ngsee/invoker/RemoteFetcher.java
@@ -0,0 +1,21 @@
+package at.sti2.ngsee.invoker;
+
+/**
+ * <b>Purpose:</b>
+ * <br>
+ * <b>Description:</b>
+ * <br>
+ * <b>Copyright:</b>     Copyright (c) 2011 STI<br>
+ * <b>Company:</b>       STI Innsbruck<br>
+ *
+ * @author      Corneliu Stanciu<br>
+ * @version     $Id$<br>
+ * Date of creation:  13.04.2011<br>
+ * File:         $Source$<br>
+ * Modifier:     $Author$<br>
+ * Revision:     $Revision$<br>
+ * State:        $State$<br>
+ */
+public abstract class RemoteFetcher {
+	
+}
