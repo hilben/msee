d78663a24d1c27722fb85415d420532a582810a9
Alex Oberhauser <oberhauseralex@networld.to>
2011-04-30 18:08:27 +0200

    Fixing merge conflict.

--- invoker-client/src/main/java/at/sti2/ngsee/invoker_client/Application.java
+++ invoker-client/src/main/java/at/sti2/ngsee/invoker_client/Application.java
@@ -12,10 +12,10 @@ public class Application {
 		InvokerWebServiceService factory = new InvokerWebServiceService();
 		InvokerWebService service = factory.getInvokerWebServicePort();
 		System.out.println(service.getVersion());
-		
+
 		String inputData0 = "<id>http://sigimera.networld.to/instances/entries#drone_00-00-00-00-00-00</id>";
 		System.out.println(service.invoke("http://localhost:9091/groundstation-webservice/services/sigimeraEntity?wsdl", "get", inputData0));
-		
+
 //		System.out.println(service.invoke("http://localhost:9091/groundstation-webservice/services/sigimeraID?wsdl", "getDrones", null));
 	}
 
