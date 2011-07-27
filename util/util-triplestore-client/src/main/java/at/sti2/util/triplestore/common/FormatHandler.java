dc29727ee072d1bb46bd3809ba0776927bbe38f7
Alex Oberhauser <oberhauseralex@sigimera.org>
2011-07-27 12:25:31 +0200

    Licensing the project under LGPLv3.

--- util/util-triplestore-client/src/main/java/at/sti2/util/triplestore/common/FormatHandler.java
+++ util/util-triplestore-client/src/main/java/at/sti2/util/triplestore/common/FormatHandler.java
@@ -1,23 +1,19 @@
 /**
- * util-sesame-client - to.networld.util.data.sesame.common
+ * Copyright (C) 2011 STI Innsbruck, UIBK
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  *
- * Copyright (C) 2011 by Networld Project
- * Written by Alex Oberhauser <oberhauseralex@networld.to>
- * All Rights Reserved
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation, version 3 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this software.  If not, see <http://www.gnu.org/licenses/>
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package at.sti2.util.triplestore.common;
 
 import org.openrdf.rio.RDFFormat;
