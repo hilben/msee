726a2ddb00ba9717ffcba5cf042ed523c13302fd
Alex Oberhauser <alex.oberhauser@sti2.at>
2012-04-10 16:28:35 +0200

    Implementing a first performance measurement that takes into account the size of the input xml file. Changing the logging of the Grounding Service from DEBUG to ERROR (much faster for big input files).

--- grounding/performance-test/client/grounding-client.rb
+++ grounding/performance-test/client/grounding-client.rb
@@ -25,12 +25,53 @@ class GroundingClient
         end
     end
 
+    def get_node_count
+        reader = Nokogiri::XML(@data)
+        nodes = reader.xpath("//*")
+        nodes.count
+    end
+
     def output_performance_measurements(response)
         xml = response.xpath("//return")[0]
         puts "Total (server-side) Execution Time   : #{xml.xpath('//totalExecutionTime/text()')} milliseconds"
         puts "|--> Input to Ontology               : #{xml.xpath('//input2OntologyExecutionTime/text()')} milliseconds"
         puts "|--> Ontology to Output              : #{xml.xpath('//ontology2OutputExecutionTime/text()')} milliseconds"
+    end
 
+    def average_execution(x_times)
+        client = Savon::Client.new do
+            wsdl.document = WSDL_URL
+        end
+
+        soapData = @data
+        xsltOntology = @xslt_to_ontology_url
+        xsltOutput = @xslt_to_output_url
+
+        totalAverage = 0
+        liftingAverage = 0
+        loweringAverage = 0
+        for i in 1..x_times
+            response = client.request :transform_debug do
+                soap.input = ["gr:transform_debug", { "xmlns:gr" => "http://sesa.sti2.at/services/" } ]
+                soap.body = {
+                    "inputMessage" => soapData,
+                    "xsltToOntologyURL" => xsltOntology,
+                    "xsltToOutputURL" => xsltOutput
+                }
+                soap.element_form_default = :unqualified
+            end
+            xml = response.xpath("//return")[0]
+            totalAverage += xml.xpath('//totalExecutionTime/text()').to_s.to_i
+            liftingAverage += xml.xpath('//input2OntologyExecutionTime/text()').to_s.to_i
+            loweringAverage += xml.xpath('//ontology2OutputExecutionTime/text()').to_s.to_i
+        end
+        totalAverage /= x_times
+        liftingAverage /= x_times
+        loweringAverage /= x_times
+        puts "Average values with #{x_times} cycles"
+        puts "Total (server-side) Execution Time (Average) : #{totalAverage} milliseconds"
+        puts "|--> Input to Ontology (Average})            : #{xml.xpath(liftingAverage)} milliseconds"
+        puts "|--> Ontology to Output (Average)            : #{xml.xpath(loweringAverage)} milliseconds"
     end
 
     def execute
@@ -53,8 +94,18 @@ class GroundingClient
     end
 end
 
-grounding = GroundingClient.new("gs1_example.xml", "http://localhost:3000/efreight/gs1_tep_demonstration/gs12ontology.xslt", "http://localhost:3000/efreight/gs1_tep_demonstration/ontology2tep.xslt")
-grounding2 = GroundingClient.new("tep_example.xml", "http://localhost:3000/efreight/gs1_tep_demonstration/tep2ontology.xslt", "http://localhost:3000/efreight/gs1_tep_demonstration/ontology2gs1.xslt")
+groundingGS1 = GroundingClient.new("perf_xml/gs1_example.xml", "http://localhost:3000/efreight/gs1_tep_demonstration/gs12ontology.xslt", "http://localhost:3000/efreight/gs1_tep_demonstration/ontology2tep.xslt")
+groundingGS1.output_performance_measurements(groundingGS1.execute)
+puts groundingGS1.get_node_count
+for i in 1..4
+    groundingGS1 = GroundingClient.new("perf_xml/gs1_example_#{i}.xml", "http://localhost:3000/efreight/gs1_tep_demonstration/gs12ontology.xslt", "http://localhost:3000/efreight/gs1_tep_demonstration/ontology2tep.xslt")
+    groundingGS1.average_execution(4)
+    puts groundingGS1.get_node_count
+end
+
+#groundingTEP = GroundingClient.new("tep_example.xml", "http://localhost:3000/efreight/gs1_tep_demonstration/tep2ontology.xslt", "http://localhost:3000/efreight/gs1_tep_demonstration/ontology2gs1.xslt")
+#groundingTEP.output_performance_measurements(groundingTEP.execute)
+#puts groundingTEP.get_node_count
 
 #time = Benchmark.bmbm do |t|
 #    t.report("GS1 -> TEP") { grounding.output_performance_measurements(grounding.execute) }
@@ -63,10 +114,10 @@ grounding2 = GroundingClient.new("tep_example.xml", "http://localhost:3000/efrei
 #puts "-----------------------"
 #puts time
 
-Benchmark.benchmark(CAPTION, 7, FORMAT, ">avg:") do |t|
-    for i in 1..100 do
-        time = t.report("GS1 -> TEP") {
-            grounding.output_performance_measurements(grounding.execute)
-        }
-    end
-end
+#Benchmark.benchmark(CAPTION, 7, FORMAT, ">avg:") do |t|
+#    for i in 1..100 do
+#        time = t.report("GS1 -> TEP") {
+#            grounding.output_performance_measurements(grounding.execute)
+#        }
+#    end
+#end
