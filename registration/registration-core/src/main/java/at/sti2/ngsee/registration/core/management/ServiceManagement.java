8764afd5ea7106e333e15a1485d5d34baa19397a
Benjamin Hiltpolt <benjamin.hiltpolt@sti2.at>
2012-12-12 17:11:02 +0100

    Fixed??? dependency problem registration core

--- registration/registration-core/src/main/java/at/sti2/ngsee/registration/core/management/ServiceManagement.java
+++ registration/registration-core/src/main/java/at/sti2/ngsee/registration/core/management/ServiceManagement.java
@@ -10,49 +10,56 @@ import at.sti2.util.triplestore.RepositoryHandler;
 
 public abstract class ServiceManagement {
 	private static RepositoryHandler reposHandler;
-	
+
 	public static void main(String[] args) throws RegistrationException {
 		delete("file:///tmp/crs");
 	}
 
-	public static String delete(String _serviceURI) throws RegistrationException {
+	public static String delete(String _serviceURI)
+			throws RegistrationException {
 		// Initialising the repository
 		ServiceManagement.initRepo();
 		try {
-			//Delete the graph from repository and shut it down
+			// Delete the graph from repository and shut it down
 			reposHandler.deleteContext(_serviceURI);
 			reposHandler.shutdown();
-			
-			return _serviceURI;			
+
+			return _serviceURI;
 		} catch (RepositoryException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
-	public static String update(String _oldServiceURI, String _newServiceURI) throws RegistrationException {
+
+	public static String update(String _oldServiceURI, String _newServiceURI)
+			throws RegistrationException {
 		// Initialising the repository
 		ServiceManagement.initRepo();
 
-		//Update through delete and add functionality
-		String registredServiceURI = TransformationWSDL.transformWSDL(_newServiceURI);
-		if ( registredServiceURI != null ) {
+		// Update through delete and add functionality
+		String registredServiceURI = TransformationWSDL
+				.transformWSDL(_newServiceURI);
+		if (registredServiceURI != null) {
 			ServiceManagement.delete(_oldServiceURI);
 			return registredServiceURI;
-		}		
+		}
 		return null;
 	}
-	
+
 	/**
 	 * Initialising the repository needed for all the above methods.
+	 * 
 	 * @throws ManagementException
 	 */
 	private static void initRepo() throws RegistrationException {
 		try {
 			Config cfg = new Config();
-			reposHandler = new RepositoryHandler(cfg.getSesameEndpoint(), cfg.getSesameReposID(),false);
+			reposHandler = new RepositoryHandler(cfg.getSesameEndpoint(),
+					cfg.getSesameReposID(), false);
 		} catch (IOException e) {
-			throw new RegistrationException("The repository endpoint ID or the WSDL file could NOT be found.", e.getCause());
+			throw new RegistrationException(
+					"The repository endpoint ID or the WSDL file could NOT be found.",
+					e.getCause());
 		}
 	}
 }
