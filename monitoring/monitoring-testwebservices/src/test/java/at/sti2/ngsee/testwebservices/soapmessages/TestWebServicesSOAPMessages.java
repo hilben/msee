4299fdbd17694bc4d4c6f65916bd0aec29285de2
Benjamin Hiltpolt <benjamin.hiltpolt@sti2.at>
2013-01-22 19:07:36 +0100

    Forgot some file in previous commit

---
+++ monitoring/monitoring-testwebservices/src/test/java/at/sti2/ngsee/testwebservices/soapmessages/TestWebServicesSOAPMessages.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package at.sti2.ngsee.testwebservices.soapmessages;
+
+/**
+ * @author Benjamin Hiltpolt
+ * 
+ */
+public class TestWebServicesSOAPMessages {
+
+	public static final String getSoap(String action, String str) {
+		return "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://testws.monitoring.ngsee.sti2.at/\"> <soapenv:Header/><soapenv:Body> <ser:"+action+"><inputString>"
+				+ str
+				+ "</inputString></ser:"+action+"></soapenv:Body></soapenv:Envelope>";
+	}
+
+}
