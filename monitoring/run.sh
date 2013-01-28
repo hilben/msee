0cef3ff903bd2531afe285cae8d1aac4f80320be
Benjamin Hiltpolt <benjamin.hiltpolt@sti2.at>
2013-01-28 15:19:12 +0100

    added tomcat plugin to deploy to sesa server

--- monitoring/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/home/koni/programs/maven/apache-maven-3.0.3/bin/mvn clean package install; \
-        cd monitoring-webservice; \
-        /home/koni/programs/maven/apache-maven-3.0.3/bin/mvn jetty:run; \
-        cd ..
