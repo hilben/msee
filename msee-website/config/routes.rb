7d8732a763a6300bae21fc62dddb3d833f4d4a98
Christian Mayr <christian.mayr@sti2.at>
2013-02-13 09:52:59 +0100

    cleanup in dashboard routes

--- msee-website/config/routes.rb
+++ msee-website/config/routes.rb
@@ -1,6 +1,11 @@
 SesaWebsite::Application.routes.draw do
 
   resources :users
+
+
+  root :to => "home#index"
+  match "dashboards/registration" => "dashboards/registration#index"
+  match "dashboards/discovery" => "dashboards/discoveries#index"
   
   match "monitorings" => "monitorings#index"
   get "monitorings/index"
@@ -18,28 +23,10 @@ SesaWebsite::Application.routes.draw do
   
   match "monitorings/qoSParamsRanking" => "monitorings#getQoSParamKeysRanking"
 
-
-
   #Json services
   match "monitorings/getSubcategoriesAndServices/:category" => "monitorings#getSubcategoriesAndServices"
 
-
-  
-  root :to => "home#index"
-  get "home/index"
-    
-  match "dashboards/grounding" => "dashboards/grounding#index"
-  get "dashboards/grounding/index"
-
-  match "dashboards/registration" => "dashboards/registration#index"
-  get "dashboards/registration/index"
-    
-  match "dashboards/discovery" => "dashboards/discoveries#index"
-  get "dashboards/discoveries/index"
-    
- 
   match "release" => "release#index"
-  get "release/index"
 
   match "doc" => "doc#index"
   namespace :doc do
@@ -52,71 +39,14 @@ SesaWebsite::Application.routes.draw do
           get "transformation/index"
       end
   end
-    
-    get "services/:id/update", :controller => "services", :action => "update"
-    get "services/:id/destroy", :controller => "services", :action => "destroy"
-    
-    get "ontologies/:id/update", :controller => "ontologies", :action => "update"
-    get "ontologies/:id/destroy", :controller => "ontologies", :action => "destroy"
-    match "ontologies/add" => "ontologies#create"
-
-    match "faq" => "faq#index"
-    get "faq/index"
-
-    match "about" => "about#index"
-    get "about/index"
-
-    match "development" => "development#index"
-    get "development/index"
-
-    match "msee" => "msee#index"
-    namespace :msee do
-        get "index"
-    end
-  
 
 
-  #resources :monitorings
+  get "services/:id/update", :controller => "services", :action => "update"
+  get "services/:id/destroy", :controller => "services", :action => "destroy"
 
-  resources :services, :only => [:index, :show, :update, :delete]
-  resources :ontologies, :only => [:index, :show, :create, :update, :delete]
-
-  devise_for :platform_owners
-  resources :platform_owners, :only => [:show, :edit, :update]
-
-  devise_for :service_owners
-  resources :service_owners, :only => [:show, :edit, :update]
-
-  get "shared/index"
-
-  get "transformation/index"
-
-  root :to => "home#index"
-  get "home/index"
-
-  match "dashboards/grounding" => "dashboards/grounding#index"
-  get "dashboards/grounding/index"
-
-  match "dashboards/registration" => "dashboards/registration#index"
-  get "dashboards/registration/index"
-
-  match "dashboards/discovery" => "dashboards/discoveries#index"
-  get "dashboards/discoveries/index"
-
-  match "release" => "release#index"
-  get "release/index"
-
-  match "doc" => "doc#index"
-  namespace :doc do
-    get "index"
-    get "service_annotation"
-    namespace :efreight do
-      match "ontology" => "ontology#index"
-      get "ontology/index"
-      match "transformation" => "transformation#index"
-      get "transformation/index"
-    end
-  end
+  get "ontologies/:id/update", :controller => "ontologies", :action => "update"
+  get "ontologies/:id/destroy", :controller => "ontologies", :action => "destroy"
+  match "ontologies/add" => "ontologies#create"
 
   get "services/:id/update", :controller => "services", :action => "update"
   get "services/:id/destroy", :controller => "services", :action => "destroy"
@@ -134,9 +64,130 @@ SesaWebsite::Application.routes.draw do
   match "development" => "development#index"
   get "development/index"
 
-  match "msee" => "msee#index"
-  namespace :msee do
-    get "index"
-  end
+  resources :services, :only => [:index, :show, :update, :delete]
+  resources :ontologies, :only => [:index, :show, :create, :update, :delete]
+
+  devise_for :platform_owners
+  resources :platform_owners, :only => [:show, :edit, :update]
+
+  devise_for :service_owners
+  resources :service_owners, :only => [:show, :edit, :update]
+
+
+
+
+
+ #  get "home/index"
+    
+ #  match "dashboards/grounding" => "dashboards/grounding#index"
+ #  get "dashboards/grounding/index"
+
+ #  match "dashboards/registration" => "dashboards/registration#index"
+ #  get "dashboards/registration/index"
+    
+ #  match "dashboards/discovery" => "dashboards/discoveries#index"
+ #  get "dashboards/discoveries/index"
+    
+ 
+ #  match "release" => "release#index"
+ #  get "release/index"
+
+ #  match "doc" => "doc#index"
+ #  namespace :doc do
+ #      get "index"
+ #      get "service_annotation"
+ #      namespace :efreight do
+ #          match "ontology" => "ontology#index"
+ #          get "ontology/index"
+ #          match "transformation" => "transformation#index"
+ #          get "transformation/index"
+ #      end
+ #  end
+    
+ #    get "services/:id/update", :controller => "services", :action => "update"
+ #    get "services/:id/destroy", :controller => "services", :action => "destroy"
+    
+ #    get "ontologies/:id/update", :controller => "ontologies", :action => "update"
+ #    get "ontologies/:id/destroy", :controller => "ontologies", :action => "destroy"
+ #    match "ontologies/add" => "ontologies#create"
+
+ #    match "faq" => "faq#index"
+ #    get "faq/index"
+
+ #    match "about" => "about#index"
+ #    get "about/index"
+
+ #    match "development" => "development#index"
+ #    get "development/index"
+
+ #  #  match "msee" => "msee#index"
+ #   # namespace :msee do
+ #   #     get "index"
+ #  #  end
+  
+
+
+ #  #resources :monitorings
+
+ #  resources :services, :only => [:index, :show, :update, :delete]
+ #  resources :ontologies, :only => [:index, :show, :create, :update, :delete]
+
+ #  devise_for :platform_owners
+ #  resources :platform_owners, :only => [:show, :edit, :update]
+
+ #  devise_for :service_owners
+ #  resources :service_owners, :only => [:show, :edit, :update]
+
+ #  get "shared/index"
+
+ #  get "transformation/index"
+
+ #  root :to => "home#index"
+ #  get "home/index"
+
+ #  #match "dashboards/grounding" => "dashboards/grounding#index"
+ #  #get "dashboards/grounding/index"
+
+ #  #match "dashboards/registration" => "dashboards/registration#index"
+ #  #get "dashboards/registration/index"
+
+ #  #match "dashboards/discovery" => "dashboards/discoveries#index"
+ #  #get "dashboards/discoveries/index"
+
+ #  match "release" => "release#index"
+ #  get "release/index"
+
+ #  match "doc" => "doc#index"
+ #  namespace :doc do
+ #    get "index"
+ #    get "service_annotation"
+ #    namespace :efreight do
+ #      match "ontology" => "ontology#index"
+ #      get "ontology/index"
+ #      match "transformation" => "transformation#index"
+ #      get "transformation/index"
+ #    end
+ #  end
+
+ #  get "services/:id/update", :controller => "services", :action => "update"
+ #  get "services/:id/destroy", :controller => "services", :action => "destroy"
+
+ #  get "ontologies/:id/update", :controller => "ontologies", :action => "update"
+ #  get "ontologies/:id/destroy", :controller => "ontologies", :action => "destroy"
+ #  match "ontologies/add" => "ontologies#create"
+
+ #  match "faq" => "faq#index"
+ #  get "faq/index"
+
+ #  match "about" => "about#index"
+ #  get "about/index"
+
+ #  match "development" => "development#index"
+ #  get "development/index"
+
+ #  #match "msee" => "msee#index"
+ # # namespace :msee do
+ # #   get "index"
+ # # end
 
 end
