8745e9c44478a77c118f50b7e8875799d203681d
Christian Mayr <christian.mayr@sti2.at>
2013-02-19 10:56:23 +0100

    RESOLVED - task 81: Precompilation of front end files http://redmine.sesa.sti2.at/issues/show/81

--- msee-website/config/environments/production.rb
+++ msee-website/config/environments/production.rb
@@ -9,7 +9,7 @@ SesaWebsite::Application.configure do
         config.action_controller.perform_caching = true
 
         # Disable Rails's static asset server (Apache or nginx will already do this)
-        config.serve_static_assets = false
+        ###config.serve_static_assets = false
 
         # Compress JavaScripts and CSS
         config.assets.compress = true
@@ -19,6 +19,12 @@ SesaWebsite::Application.configure do
 
         # Generate digests for assets URLs
         config.assets.digest = true
+
+
+        # For faster precompilation
+        # @see http://guides.rubyonrails.org/asset_pipeline.html#precompiling-assets
+        config.assets.initialize_on_precompile = false
+
 
         # Defaults to Rails.root.join("public/assets")
         # config.assets.manifest = YOUR_PATH
@@ -48,6 +54,7 @@ SesaWebsite::Application.configure do
 
         # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
         # config.assets.precompile += %w( search.js )
+        config.assets.precompile += ['*.js', '*.css']
 
         # Disable delivery errors, bad email addresses will be ignored
         # config.action_mailer.raise_delivery_errors = false
