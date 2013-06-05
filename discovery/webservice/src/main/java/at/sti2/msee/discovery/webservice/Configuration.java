01b0d33215c0bd2018ae63c2b7c0027c322faa1c
Christian Mayr <christian.mayr@sti2.at>
2013-06-05 12:09:04 +0200

    RESOLVED - task 209: Create test with on-the-fly axis2 web server for Discovery http://redmine.sesa.sti2.at/issues/show/209

--- discovery/webservice/src/main/java/at/sti2/msee/discovery/webservice/Configuration.java
+++ discovery/webservice/src/main/java/at/sti2/msee/discovery/webservice/Configuration.java
@@ -1,6 +1,7 @@
 package at.sti2.msee.discovery.webservice;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class Configuration {
@@ -9,24 +10,50 @@ public class Configuration {
 
 	public Configuration() throws IOException {
 		this.properties = new Properties();
+		load();
 	}
 
 	public void load() throws IOException {
-		this.properties.load(Configuration.class
-				.getResourceAsStream(propertyLocation));
+		this.properties.load(this.getClass().getResourceAsStream(propertyLocation));
 	}
 
 	public void load(String propertyLocation) throws IOException {
 		this.propertyLocation = propertyLocation;
-		this.properties.load(Configuration.class
-				.getResourceAsStream(propertyLocation));
+		load();
+	}
+
+	public void load(InputStream resourceAsStream) {
+		try {
+			properties.load(resourceAsStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public String getSesameEndpoint() {
-		return this.properties.getProperty("discovery.sesame.endpoint");
+		String endpoint = this.properties.getProperty("discovery.sesame.endpoint");
+		if (endpoint == null) {
+			throw new IllegalArgumentException(endpoint);
+		}
+		return endpoint;
 	}
 
 	public String getSesameReposID() {
-		return this.properties.getProperty("default.sesame.repository.name");
+		String repository = this.properties.getProperty("default.sesame.repository.name");
+		if (repository == null) {
+			repository = this.properties.getProperty("discovery.sesame.reposid");
+		}
+		if (repository == null) {
+			throw new IllegalArgumentException(
+					"\"default.sesame.repository.name\" or \"discovery.sesame.reposid\" not set");
+		}
+		return repository;
 	}
+
+	@Override
+	public String toString() {
+		return "Configuration [propertyLocation=" + propertyLocation + ", endp="
+				+ getSesameEndpoint() + ", repo=" + getSesameReposID() + "]";
+	}
+
 }
